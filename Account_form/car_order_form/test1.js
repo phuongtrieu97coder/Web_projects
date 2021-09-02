fd36dd716e486d4e30fae5256a6aead68012b32e
phuongtrieu97coder <82598726+phuongtrieu97coder@users.noreply.github.com>
2021-09-02 15:24:36 -0700

    Update test1.js

--- Account_form/car_order_form/test1.js
+++ Account_form/car_order_form/test1.js
@@ -2,7 +2,7 @@
 
 
 
-        document.getElementById("car_img_item").src = "image/car/car_icon.png";
+        document.getElementById("car_img_item").src = "img/car/car_icon.png";
         document.getElementById("car_name_intro").innerHTML = "CAR NAME";
         document.getElementById("car_price_intro").innerHTML = "CAR PRICE"; 
 
