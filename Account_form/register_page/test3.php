a704a7bd5e8754cb818d741346fc5b75def0ab7c
phuongtrieu97coder <82598726+phuongtrieu97coder@users.noreply.github.com>
2021-09-05 17:02:45 -0700

    Create test3.php

---
+++ Account_form/register_page/test3.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+<link rel="stylesheet" type='text/css' href="Sass_Css_file/test1.css">
+
+
+    </head>
+
+
+
+
+    <body>
+  
+    <div class="w3-content">
+                 <b style="text-align:center">SEARCH RESULT: <?php echo $_POST["search1"]?></b>
+    </div>
+      
+    </body>
+
+
+
+    <footer></footer>
+</html>
