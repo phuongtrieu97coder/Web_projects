773469818094c8fd61b34f340446b5fe375552b2
phuongtrieu97coder <82598726+phuongtrieu97coder@users.noreply.github.com>
2022-03-24 11:28:02 -0700

    Add files via upload

---
+++ Account_form/sign_up_form1/sign_up_form1.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <head>
+     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+     
+     <title>PHUONG WEB</title>
+
+     <link rel="shortcut icon"   href="image/js_logo.png" type="image/x-icon">
+
+     <link rel="stylesheet" type="text/css"  href="test2.css">
+           
+  
+
+
+         <style>
+        
+         
+         </style>
+    </head>
+
+
+    <body>
+
+      <header>
+    
+      </header>
+
+     <main>
+
+
+      <form action="test1.php" method="POST" style="border:2px solid blue;padding:20px;width:500px;">
+         
+        
+           <div style="text-align:center;font-size:40px;font-weight:700">
+               <p><i class="fa fa-user-circle-o" style="color:yellowgreen"></i>  SIGN UP </p>
+           </div>
+
+
+
+           <label for="name_input"><b>Name :</b></label><br>
+            <input type="text" id="name_input" name="name" style="width:100%"><br>
+
+           <label for="age_input"><b>Age :</b></label><br>
+           <input type="number" id="age_input" name="age" style="width:100%"><br>
+
+
+           <label for="email_input"><b>Email :</b></label><br>
+           <input type="email" id="email_input" name="email" style="width:100%"><br>
+
+           <label for="password_input"><b>Password :</b></label><br>
+           <input type="password" id="password_input" name="password" style="width:100%"><br>
+
+           <br>
+
+           <input type="checkbox" id="male_check" name="male">
+           <label for="male_check">Male</label>
+
+
+           <input type="checkbox" id="female_check" name="female">
+           <label for="female_check">Female</label>
+
+           <br><br>
+
+           <input type="submit" name="submit" class="btn btn-dark text-white w3-hover-aqua">
+      </form>
+        
+      </main>
+ 
+   
+       
+
+
+
+     
+      
+     
+      
+    </body>
+
+
+   
+
+
+    <footer>
+
+
+    </footer>
+
+
+
+
+</html>
+        
+
+
+
+
+
+
+
+
---
+++ Account_form/sign_up_form1/test1.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <head>
+     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+   
+   
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  
+  
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   
+   
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  
+  
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+     
+
+           
+  
+
+
+         <style>
+           
+         </style>
+    </head>
+
+
+    <body >
+
+    <header>
+          <nav class="w3-top w3-bar w3-black">
+              <button class="w3-bar-item w3-button w3-ripple w3-hover-aqua">HOME</button>
+              <button class="w3-bar-item w3-button w3-ripple w3-hover-aqua">MENU</button>
+              <button class="w3-bar-item w3-button w3-ripple w3-hover-aqua">EMAIL</button>
+              
+              <div class="w3-right">
+                 <span class="w3-bar-item">
+                 <p>WELCOME <?php echo $_POST["name"]; ?></p> 
+                 </span>
+
+                 <span class="w3-bar-item">
+                     <b><?php  echo $_POST["email"]; ?></b>
+                 </span>
+                
+                  <button class="w3-bar-item w3-button w3-ripple w3-hover-aqua">LOGOUT</button>
+                  <button class="w3-bar-item w3-button w3-ripple w3-hover-aqua">ABOUT</button>
+              </div>
+          </nav>
+    </header>
+    
+    <div id="main_content">
+               
+    
+    <div id="block1"></div>
+    <div id="block2"></div>
+   
+      <script>
+      
+ 
+    
+        </script>
+
+        
+
+
+
+    
+          
+      
+     
+            
+     
+
+
+
+
+
+    </div>
+
+      
+    </body>
+
+
+   
+
+
+    <footer>
+
+    </footer>
+
+
+
+
+</html>
+        
